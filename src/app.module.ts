0ba08a33a53ee2cb45ef290b22538a7bda1f5da3
Erick Karl <erickkarl5@gmail.com>
2025-09-09 19:43:24 -0300

    fix: api global prefix set, prescription module importing and linting fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrescriptionModule } from './prescription/prescription.module';
 
 @Module({
-  imports: [],
+  imports: [PrescriptionModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prescription/prescription.service.ts
+++ src/prescription/prescription.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { PrescriptionProcessingDTOIn } from './dto/prescription.dto';
+import {
+  PrescriptionProcessingDTOIn,
+  PrescritionProcessingDTOOut,
+} from './dto/prescription.dto';
 
 @Injectable()
 export class PrescriptionService {
   async prescritionProcessing(
     input: PrescriptionProcessingDTOIn,
-  ): Promise<any> {
+  ): Promise<PrescritionProcessingDTOOut> {
     console.log(input.file);
-    return Promise.resolve('Ok');
+    return Promise.resolve({});
   }
 }
