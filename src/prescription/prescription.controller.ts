4e74760a0091673039a9b99b8ac2aa6002636ff9
Erick Karl <erickkarl5@gmail.com>
2025-09-10 03:42:29 -0300

    feat(wip): storing files on a local folder to better simulate cloud storing

--- src/prescription/prescription.controller.ts
+++ src/prescription/prescription.controller.ts
@@ -6,13 +6,21 @@ import {
 } from '@nestjs/common';
 import { PrescriptionService } from './prescription.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('prescription')
 export class PrescriptionController {
   constructor(private readonly prescriptionService: PrescriptionService) {}
 
   @Post('/upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    // Escolhi armazenar o arquivo para simular melhor como seria um upload real (ex: S3, etc)
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+      }),
+    }),
+  )
   async processing(@UploadedFile() file: Express.Multer.File) {
     const result = await this.prescriptionService.prescritionProcessing({
       file,
