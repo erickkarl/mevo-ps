5991ca2f4d2696b2dc0734f5193e3ddf3a5006c5
Erick Karl <erickkarl5@gmail.com>
2025-09-10 00:14:34 -0300

    feat(wip): implementing bullmq for background processing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrescriptionModule } from './prescription/prescription.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [PrescriptionModule],
+  imports: [
+    PrescriptionModule,
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT ?? '6379'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prescription/prescription.module.ts
+++ src/prescription/prescription.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrescriptionController } from './prescription.controller';
 import { PrescriptionService } from './prescription.service';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [],
+  imports: [
+    /* Decidi usar o BullMQ para fazer a fila de processamento em background pois 
+    o Nest já possui suporte e encontrei vários exemplos de uso */
+    BullModule.registerQueue({
+      name: 'prescription',
+    }),
+  ],
   controllers: [PrescriptionController],
   providers: [PrescriptionService],
 })
